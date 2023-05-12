211dd4925015cecf916425ddd6130fc36e14f496
Amit Kedia <amitkedia3000@gmail.com>
2023-05-12 13:36:16 +0100

    Hyperparameter Tuning Implementation

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,8 +47,50 @@ class ModelTrainer:
             "AdaBoost Regressor": AdaBoostRegressor()
             }
 
-            model_report:dict = evaluate_models(X_train= X_train, y_train = y_train, X_test = X_test,
-             y_test = y_test, models = models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "Lasso": {},  
+                "Ridge": {},
+                "K-Neighbors Regressor": {},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report, updated_models = evaluate_models(X_train= X_train, y_train = y_train, X_test = X_test,
+            y_test = y_test, models = models, params=params)
+            models = updated_models
             
             ## To get the best model from the dict
             best_model_score = max(sorted(model_report.values()))
@@ -88,3 +130,4 @@ class ModelTrainer:
 
         except Exception as e:
             raise CustomExeption(e, sys)
+        
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from src.exception import CustomExeption
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.metrics import r2_score
 
@@ -20,28 +21,39 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomExeption(e,sys)
 
-def evaluate_models(X_train, y_train,X_test, y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
             model = list(models.values())[i]
-            model.fit(X_train, y_train) # Train model
+            
+            if params[model_name]:  # If there are parameters to tune for this model
+                grid_search = GridSearchCV(model, params[model_name], cv=5)
+                grid_search.fit(X_train, y_train)
+                model = grid_search.best_estimator_  # Use the best estimator found by GridSearchCV
+            
+            else:  # If there are no parameters to tune, just fit the model
+                model.fit(X_train, y_train)  
+
+            # Update the model in the dictionary with the fitted model
+            models[model_name] = model
 
             # Make predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
         
-        return report
-    
+        return report, models  # return the models dictionary as well
+
     except Exception as e:
-        raise CustomExeption(e,sys)
+        raise CustomExeption(e, sys)
+
     
 def load_object(file_path):
     try:
